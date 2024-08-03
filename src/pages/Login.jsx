4a6a255105607278f4ace51751a60a174bd1ee5a
piluvz <dilnaz.rakhmankulova@gmail.com>
2024-08-03 20:46:41 +0500

    new

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../style/Login.css';
 
 const Login = () => {
   return (
-    <div>
+    <div className='login-cont'>
       <h1>Login Page</h1>
       <p>Login form will go here</p>
       <Link to="/">Back to Home</Link>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../style/Login.css';
 
 const Register = () => {
   return (
-    <div>
+    <div className='login-cont'>
       <h1>Register Page</h1>
       <p>Registration form will go here</p>
       <Link to="/">Back to Home</Link>
